7bd1655f678bff46ea0e08fb89e61d0cebd5c26f
prakhar-2005 <prakharvarshney2005@gmail.com>
2023-03-04 21:35:52 +0530

    firebase auth

--- app/src/main/java/com/example/weathernowapp/MainActivity.kt
+++ app/src/main/java/com/example/weathernowapp/MainActivity.kt
@@ -4,16 +4,52 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import com.example.weathernowapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val signuptext: TextView = findViewById(R.id.signuptext)
-
-        signuptext.setOnClickListener{
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signuptext.setOnClickListener{
             val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginbutton.setOnClickListener{
+            val email = binding.emailbox.text.toString()
+            val pass = binding.passwordbox.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity2::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Credentials are wrong or user doesn't exist", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+            } else {
+                Toast.makeText(this, "Empty Fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/weathernowapp/SignupActivity.kt
+++ app/src/main/java/com/example/weathernowapp/SignupActivity.kt
@@ -3,19 +3,45 @@ package com.example.weathernowapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
+import com.example.weathernowapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
 
-        val logintext: TextView = findViewById(R.id.logintxt)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        logintext.setOnClickListener{
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.logintxt.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        binding.signupbutton.setOnClickListener {
+            val email = binding.emailbox.text.toString()
+            val pass = binding.passwordbox.text.toString()
 
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+            } else {
+                Toast.makeText(this, "Empty Fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
